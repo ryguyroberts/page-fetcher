d3a51125c766fcafaebfddd674ba4b74a96c1fc2
Ryan Robertson <ryguyrobertson@gmail.com>
2024-01-17 13:51:46 -0800

    Added checks for bad URL and bad FilePath

--- fetcher.js
+++ fetcher.js
@@ -15,10 +15,15 @@ request(URL, (error, response, body) => {
   if (response.statusCode !== 200) {
     // Print the response status code if an error. 200 Normal response
     console.log('statusCode:', response && response.statusCode); 
+    console.log(`Something wrong with your URL not outputting to file`);
+    return;
   }
   fs.writeFile(filePath, body, (error) => {
+    // if file path is wrong
     if (error) {
-      return console.log(error);
+      console.log("File Path incorrect");
+      console.log(error);
+      return;
     }
     // Get the file size put output message
     fs.stat(filePath, (error, stats) => {
