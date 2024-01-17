ef53292b743073ae827d0680395b6e81cd483837
Ryan Robertson <ryguyrobertson@gmail.com>
2024-01-17 13:54:01 -0800

    Lint checkin changes

--- fetcher.js
+++ fetcher.js
@@ -1,12 +1,12 @@
 // Slice first to remove first 2 process.arg vals
 const slicedArr = process.argv.splice(2);
-const URL = slicedArr[0];;
+const URL = slicedArr[0];
 const filePath = slicedArr[1];
 
 const fs = require('fs');
 const request = require('request');
 
-// function to get body of a page. 
+// function to get body of a page.
 request(URL, (error, response, body) => {
   if (error) {
     // Print the error if one occurred
@@ -14,7 +14,7 @@ request(URL, (error, response, body) => {
   }
   if (response.statusCode !== 200) {
     // Print the response status code if an error. 200 Normal response
-    console.log('statusCode:', response && response.statusCode); 
+    console.log('statusCode:', response && response.statusCode);
     console.log(`Something wrong with your URL not outputting to file`);
     return;
   }
@@ -30,7 +30,7 @@ request(URL, (error, response, body) => {
       if (error) {
         console.log(error);
       }
-        console.log(`Downloaded and saved ${stats.size} bytes to ${filePath}`);
+      console.log(`Downloaded and saved ${stats.size} bytes to ${filePath}`);
     });
-  })
+  });
 });
